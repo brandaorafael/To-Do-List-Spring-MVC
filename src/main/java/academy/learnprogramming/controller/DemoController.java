1a9b919c454144f95360769d847bc918d8dc03f2
Rafael Brandao <r.augusto.brandao@gmail.com>
2019-12-31 16:35:25 -0500

    Added DemoService Interface and Implementation on the project

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @Controller
 public class DemoController {
+
+    private final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
 
     // http://localhost:8080/To-Do-List-Spring-MVC/hello
     @ResponseBody
@@ -21,7 +30,7 @@ public class DemoController {
     // http://localhost:8080/To-Do-List-Spring-MVC/welcome
     @GetMapping("welcome")
     public String welcome(Model model){
-        model.addAttribute("user", "Rafael");
+        model.addAttribute("helloMessage", demoService.getHelloMessage("Rafael"));
         log.info("Model = {}", model);
         return "welcome";
     }
@@ -29,6 +38,6 @@ public class DemoController {
     @ModelAttribute("welcomeMessage")
     public String welcomeMessage(){
         log.info("welcomeMessage() is called");
-        return "Welcome to this Demo application!";
+        return demoService.getWelcomeMessage();
     }
 }
